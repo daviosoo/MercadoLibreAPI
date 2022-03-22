143eac91bfda291af15919a1740d7665c3d21b73
daniel arenas <arenas9808@gmail.com>
2022-03-21 20:24:53 -0500

    cambio de nombre a la db

--- features/login/login.php
+++ features/login/login.php
@@ -1,1 +1,18 @@
 <?php
+
+    require_once(dirname(__DIR__)."../../conection/db_conecton.php");        
+        
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $data= json_decode(file_get_contents('php://input'),true);
+        $identification = $data['identification'];
+        $db = new DBconfig();
+        $dbconnection  = $db->connect();
+        $query= "SELECT * FROM tabla WHERE identification='$identification'";
+        $users=$dbconnection->query($query)->fetchAll(PDO::FETCH_ASSOC);
+        header('Content-Type: application/json');
+        echo(json_encode($users));
+    }else{
+        echo "ERROR";
+    }
+
+
