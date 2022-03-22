1f7e3da78effcff12e49e6ac450bc8f007b1fb8d
daniel arenas <arenas9808@gmail.com>
2022-03-21 21:15:28 -0500

    se agrego la bd

--- features/register/register.php
+++ features/register/register.php
@@ -1,1 +1,4 @@
 <?php
+    require_once(dirname(__DIR__)."../../conection/db_connecton.php");  
+
+    INSERT INTO `usuario` (`identificacion`, `email`, `celular`, `contra`) VALUES ('1152343213', 'pepito@gmail.com', '11111111', '123456789');
