942601e2aed91d6fec1acf8a94fba5c5af6cc724
daniel arenas <arenas9808@gmail.com>
2022-03-21 21:05:25 -0500

    se hizo el endpoint del login

--- conection/db_connecton.php
+++ conection/db_connecton.php
@@ -1,1 +1,24 @@
 <?php
+
+class DBconfig{
+    private $user="root";
+    private $password="";
+    private $servidor="localhost";
+    private $nameDB="mercadolibrebd";
+
+    public function connect(){
+        try{        
+            $dns="mysql:host=$this->servidor;dbname=$this->nameDB";
+            $connection= new PDO($dns,$this->user,$this->password);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $connection;
+            //echo "Conexion Exitosa!!!!";
+        }catch(PDOException $err){
+            echo "Error en la Base de datos".$err->getMessage();
+        }
+    }
+}
+
+
+
+
--- features/login/login.php
+++ features/login/login.php
@@ -1,1 +1,23 @@
 <?php
+
+    require_once(dirname(__DIR__)."../../conection/db_connecton.php");        
+        
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $data= json_decode(file_get_contents('php://input'),true);
+        $email = $data['email'];
+        $contra=$data['contra'];
+        $db = new DBconfig();
+        $dbconnection  = $db->connect();
+        $query= "SELECT * FROM usuario WHERE email='$email' AND contra='$contra'";
+        $users=$dbconnection->query($query)->fetchAll(PDO::FETCH_ASSOC);
+        header('Content-Type: application/json');
+        if(empty($users)){
+            echo("ingresaste mal los datos");
+        }else{
+            echo(json_encode($users));
+        }
+    }else{
+        echo "ERROR";
+    }
+
+
